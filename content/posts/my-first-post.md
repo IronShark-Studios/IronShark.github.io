1c829d42a0809eca0cd1688bfb269928c5154ee0
xin <git@ironshark.org>
2024-05-15 13:40:25 -0400

    imported old posts

--- content/posts/my-first-post.md
+++
@@ -1,5 +0,0 @@
-+++
-title = 'My First Post'
-date = 2024-05-15T08:32:13-04:00
-draft = true
-+++
--- flake.nix
+++ flake.nix
@@ -4,20 +4,28 @@
     inputs = {
       nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
       flake-utils.url = "github:numtide/flake-utils";
+      hugo-theme = {
+        url = "github:nunocoracao/blowfish";
+        flake = false;
+      };
     };
 
-    outputs = { self, nixpkgs, flake-utils, }:
+    outputs = { self, nixpkgs, flake-utils, hugo-theme }:
       flake-utils.lib.eachDefaultSystem (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
           {
             packages.website = pkgs.stdenv.mkDerivation rec {
-              pname = "static-website";
-              version = "2021-11-19";
+              pname = "personal-website";
+              version = "2024-05-15";
               src = ./.;
-              nativeBuildInputs = [ pkgs.hugo ];
+              nativeBuildInputs = [
+                pkgs.hugo
+              ];
               configurePhase = ''
+                mkdir -p "themes/blowfish"
+                cp -r ${hugo-theme}/* "themes/blowfish"
               '';
               buildPhase = "${pkgs.hugo}/bin/hugo";
               installPhase = "cp -r public $out";
@@ -26,9 +34,13 @@
             defaultPackage = self.packages.${system}.website;
 
             devShell = pkgs.mkShell {
-              packages = [ pkgs.hugo ];
+              packages = [
+                pkgs.hugo
+              ];
               shellHook = ''
-          '';
+                mkdir -p themes
+                ln -sn "${hugo-theme}" "themes/blowfish"
+              '';
             };
           }
       );
